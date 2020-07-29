bcefd9157c2d690499d643cfdf9d1b3002d842d8
Damian Cybiak <d.cybiak@softwizard.pl>
2020-07-30 00:10:31 +0200

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,11 @@
 import React from "react"
-import {Link} from "gatsby"
+// import {Link} from "gatsby"
 // import {faReact} from "@fortawesome/free-brands-svg-icons"
 // import "typeface-muli"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
-    <Layout>
-        {/*<FontAwesomeIcon icon={faReact}/>*/}
-        <SEO title="Home"/>
-        <h1>Hi people</h1>
-        {/*<p style={{fontFamily: 'muli, sans-serif'}}>Welcome to your new Gatsby site.</p>*/}
-        <p>Now go build something great.</p>
-        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
-            <Image/>
-        </div>
-        <Link to="/page-2/">Go to page 2</Link> <br/>
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </Layout>
+   <h1>Hello</h1>
 )
 
 export default IndexPage
