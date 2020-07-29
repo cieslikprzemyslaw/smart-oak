96408b1fce17a5787e19225fc402b5ee0737d5ad
Damian Cybiak <d.cybiak@softwizard.pl>
2020-07-29 23:16:55 +0200

    Add google font "Muli" required in project + example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from "gatsby"
 // import {faReact} from "@fortawesome/free-brands-svg-icons"
+// import "typeface-muli"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -11,7 +12,7 @@ const IndexPage = () => (
         {/*<FontAwesomeIcon icon={faReact}/>*/}
         <SEO title="Home"/>
         <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
+        {/*<p style={{fontFamily: 'muli, sans-serif'}}>Welcome to your new Gatsby site.</p>*/}
         <p>Now go build something great.</p>
         <div style={{maxWidth: `300px`, marginBottom: `1.45rem`}}>
             <Image/>
