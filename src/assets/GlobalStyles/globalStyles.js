51eab589084faa3cb2ea353c2a472524e7e3f9cb
Damian Cybiak <d.cybiak@softwizard.pl>
2020-08-05 21:05:22 +0200

    Clear gatsby-config.js. Remove wrong choice of import font

--- src/assets/GlobalStyles/globalStyles.js
+++ src/assets/GlobalStyles/globalStyles.js
@@ -1,5 +1,4 @@
 import {createGlobalStyle} from 'styled-components';
-import "typeface-muli"
 
 export default createGlobalStyle`
 
@@ -19,13 +18,11 @@ html{
 body{
     margin: 0;
     padding: 0;
-    font-family: "Muli" ,sans-serif; 
 }
 
 button{
     cursor: pointer;
     padding: 0;
-    font-family: "Muli", sans-serif;
 }
 
 ul{
