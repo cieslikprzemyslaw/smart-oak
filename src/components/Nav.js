4b6e66d319351d686d8c628494c794e15afdd629
Damian Cybiak <damian.cybiak@nmholding.pl>
2020-09-01 16:21:01 +0200

    create first version of nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { FaChevronDown } from "react-icons/fa/index";
+import { Link } from 'gatsby';
+
+const NavigationWrapper = styled.nav`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    height: 41px;
+    background-color: #000;
+    
+`;
+
+const NavigationList = styled.ul`
+    margin-right: 50px;
+    list-style: none;
+`;
+
+
+const NavigationListItem = styled.li`
+      font-size: 14px;
+      font-weight: 600;
+      line-height: 41px;
+      margin-left: 50px;
+            
+      a {
+        margin-right: 15px;
+        text-decoration: none;
+        color: white;   
+      }
+`;
+
+const NavigationListItemChevronDown = styled.span`
+    margin-left: 10px;
+    
+    svg{
+    transform: translateY(20%);
+    }
+`;
+
+const NavigationListItemPercent = styled.span`
+    margin-left: 10px;
+    font-weight: 900;
+    font-size: 17px;
+`;
+
+const Navigation = () => {
+    return (
+        <>
+            <NavigationWrapper>
+                <NavigationList>
+                    <NavigationListItem>
+                        <Link to="/">Przekaż
+                            <NavigationListItemPercent>1%</NavigationListItemPercent>
+                        </Link>
+                        <Link to="/">Język: PL
+                            <NavigationListItemChevronDown><FaChevronDown /></NavigationListItemChevronDown>
+                        </Link>
+                    </NavigationListItem>
+                </NavigationList>
+            </NavigationWrapper>
+        </>
+    )
+};
+
+export default Navigation;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useIntl } from 'gatsby-plugin-intl';
 import styled from 'styled-components';
-
+import Nav from '../components/Nav';
 const StyledHeading = styled.h1`
     color: ${({ theme: { colors } }) => colors.underline1};
     font-size: ${({ theme: { fontSizes } }) => fontSizes.big};
@@ -12,6 +12,7 @@ const IndexPage = () => {
 
     return (
         <>
+        <Nav />
             <StyledHeading>
                 {intl.formatMessage({
                     id: 'homePage.test',
