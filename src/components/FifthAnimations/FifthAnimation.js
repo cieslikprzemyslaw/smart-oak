f74403973ea89125b38973b48536d9a91616ef5a
mycak <mycak@wp.pl>
2020-10-26 09:59:37 +0100

    first commit

---
+++ src/components/FifthAnimations/FifthAnimation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+const FifthAnimation = () => {
+  return (
+    <div>
+      <p>FifthAnimation</p>
+    </div>
+  )
+}
+
+
+export default FifthAnimation;
