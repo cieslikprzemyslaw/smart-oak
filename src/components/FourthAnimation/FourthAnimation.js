ce5f6f00a5fe8b041404acb38a9d5ae8947c4686
Winnicki Antoni (STUD) <01133502@pw.edu.pl>
2020-10-27 15:16:15 +0100

    [Add] component

--- src/components/FourthAnimation/FourthAnimation.js
+++ src/components/FourthAnimation/FourthAnimation.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
-// import { gsap } from 'gsap';
-// import ScrollTrigger from 'gsap/ScrollTrigger';
-// import ImageSrc from '../../assets/images/forest-background.png';
-// import { useIntl } from 'gatsby-plugin-intl';
-
-// gsap.registerPlugin(ScrollTrigger);
+import React, { useRef, useEffect } from 'react';
+import ImageSrc from '../../assets/images/forest-background-cropped.png';
+import { scaleImgOnScroll, showContent } from './Animate';
+import { ContentParagraph, Container, StyledImg, Content, ContentHeader } from './styles.js';
 
 const FourthAnimation = () => {
-    const DummySection = styled.section`
-        display: block;
-        background: black;
-        height: 700px;
-    `;
+    const imgRef = useRef(null);
+
+    useEffect(() => {
+        scaleImgOnScroll(imgRef.current);
+        showContent(imgRef.current);
+    }, []);
+
     return (
         <>
-            <DummySection />
-            Czwarta Animacja
-            <DummySection />
+            <Container>
+                <StyledImg src={ImageSrc} ref={imgRef} />
+                <Content id="fourth-animation-content">
+                    <ContentHeader>
+                        <h2>
+                            Smart Oak to przede wszystkim przestrzeń dla społeczeństwa projektowego.
+                        </h2>
+                    </ContentHeader>
+                    <ContentParagraph>
+                        <p>
+                            Znajdzie się tu miejsce dla społecznych wilków, fanów startupów,
+                            naukowców, organizatorów wydarzeń, a także przedsiębiorców. Inwestorzy,
+                            sponsorzy i aniołowie biznesu również znajdą tu ciekawe możliwości.
+                            Każdy projekt można w łatwy sposób wspierać finansowo. Platforma pozwala
+                            każdemu aktywnie udzielać się w grupach informacyjnych. Przyczyni się to
+                            do poznania innych pasjonatów, z którymi wspólnie można rozwijając swoje
+                            zainteresowania i realizować nowe projekty. System stwarza też możliwość
+                            brania działu w e-wolontariacie – każdy z odpowiednimi kompetencjami
+                            będzie mógł się zaangażować w pomoc młodzieży.
+                        </p>
+                    </ContentParagraph>
+                </Content>
+            </Container>
         </>
     );
 };
