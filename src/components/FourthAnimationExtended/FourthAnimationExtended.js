d1b991fc4db7b5ad9ba1b2a6d5522bfae5cb5af1
Winnicki Antoni (STUD) <01133502@pw.edu.pl>
2020-10-30 12:54:13 +0100

    [Add] text translation

--- src/components/FourthAnimationExtended/FourthAnimationExtended.js
+++ src/components/FourthAnimationExtended/FourthAnimationExtended.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import ImageSrc from '../../assets/images/forest-background-cropped.png';
+import { useIntl } from 'gatsby-plugin-intl';
 import { scaleImgOnScroll, pinContent } from './Animate';
 import {
     AnimationContentParagraph,
@@ -15,6 +16,7 @@ import {
 
 const FourthAnimation = () => {
     const imgRef = useRef(null);
+    const intl = useIntl();
 
     useEffect(() => {
         scaleImgOnScroll(imgRef.current);
@@ -28,20 +30,16 @@ const FourthAnimation = () => {
                 <AnimationContent id="fourth-animation-content">
                     <AnimationContentHeader>
                         <h2>
-                            Smart Oak to przede wszystkim przestrzeń dla społeczeństwa projektowego.
+                            {intl.formatMessage({
+                                id: `fourthAnimationExtendedSection.title1`,
+                            })}
                         </h2>
                     </AnimationContentHeader>
                     <AnimationContentParagraph>
                         <p>
-                            Znajdzie się tu miejsce dla społecznych wilków, fanów startupów,
-                            naukowców, organizatorów wydarzeń, a także przedsiębiorców. Inwestorzy,
-                            sponsorzy i aniołowie biznesu również znajdą tu ciekawe możliwości.
-                            Każdy projekt można w łatwy sposób wspierać finansowo. Platforma pozwala
-                            każdemu aktywnie udzielać się w grupach informacyjnych. Przyczyni się to
-                            do poznania innych pasjonatów, z którymi wspólnie można rozwijając swoje
-                            zainteresowania i realizować nowe projekty. System stwarza też możliwość
-                            brania działu w e-wolontariacie – każdy z odpowiednimi kompetencjami
-                            będzie mógł się zaangażować w pomoc młodzieży.
+                            {intl.formatMessage({
+                                id: `fourthAnimationExtendedSection.block1`,
+                            })}
                         </p>
                     </AnimationContentParagraph>
                 </AnimationContent>
@@ -49,23 +47,16 @@ const FourthAnimation = () => {
             <AfterAnimationContainer>
                 <AfterAnimationContentHeader>
                     <h2>
-                        System stwarza też możliwość brania działu w e-wolontariacie – każdy z
-                        odpowiednimi kompetencjami będzie mógł się zaangażować w pomoc młodzieży.
+                        {intl.formatMessage({
+                            id: `fourthAnimationExtendedSection.title2`,
+                        })}
                     </h2>
                 </AfterAnimationContentHeader>
                 <AfterAnimationContentParagraph>
                     <p>
-                        Dzięki takiemu rozwiązaniu swoimi kompetencjami będzie mógł wspierać każdy,
-                        kto posiada doświadczenie projektowe. Użytkownicy naszego portalu będą mogli
-                        personalizować swoje profile, czyniąc je atrakcyjniejszymi dla innych.
-                        Zwiększy to szansę na otrzymanie zaproszeń do uczestnictwa w projektach od
-                        innych grup projektowych lub pomoże znaleźć pracę marzeń. Ocena wszystkich
-                        podjętych działań na platformie będzie kończyć się z początkiem wakacji,
-                        natomiast podsumowanie odbędzie się podczas kilkudniowego wydarzenia w
-                        sierpniu. Rozdamy wtedy stypendia i wyróżnienia. Konferencja Smart Oak
-                        Projects pozwoli przedstawić ranking najlepszych projektów i szkół oraz
-                        zaprezentować najlepsze projekty od strony ich realizacji. Do zobaczenia na
-                        naszym portalu. Łap motywację i podbijaj świat projektami!
+                        {intl.formatMessage({
+                            id: `fourthAnimationExtendedSection.block2`,
+                        })}
                     </p>
                 </AfterAnimationContentParagraph>
             </AfterAnimationContainer>
