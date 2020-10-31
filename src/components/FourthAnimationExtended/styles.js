9979a9eed3b59cd84dfa273b3d82b66d807c7f82
Winnicki Antoni (STUD) <01133502@pw.edu.pl>
2020-10-31 09:54:47 +0100

    [Add] media screen styles

--- src/components/FourthAnimationExtended/styles.js
+++ src/components/FourthAnimationExtended/styles.js
@@ -20,7 +20,7 @@ const AnimationContainer = styled.div`
 const AnimationContent = styled.div`
     position: absolute;
     width: 75%;
-    top: 0;
+    top: 10%;
     left: 50%;
     transform: translate(-50%, 0);
     color: white;
@@ -30,11 +30,31 @@ const AnimationContentParagraph = styled.p`
     margin-top: 40px;
     font-size: 24px;
     line-height: 1.5;
+
+    @media screen and (max-width: 950px) {
+        font-size: 22px;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 21px;
+    }
+    @media screen and (max-width: 600px) {
+        font-size: 20px;
+    }
 `;
 
 const AnimationContentHeader = styled.h2`
     font-size: 44px;
     line-height: 1.2;
+
+    @media screen and (max-width: 950px) {
+        font-size: 33px;
+    }
+    @media screen and (max-width: 800px) {
+        font-size: 27px;
+    }
+    @media screen and (max-width: 600px) {
+        font-size: 22px;
+    }
 `;
 
 const AfterAnimationContainer = styled.div`
