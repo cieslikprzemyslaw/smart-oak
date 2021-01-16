edfeb6fd56d6276de04d5b535db265a0cdce0f29
Jakub Rejmann <kubarejmann@wp.pl>
2021-01-16 16:47:12 +0100

    Animation done

--- src/components/pages/homePage/CreativeEnvironment/useAnimate.js
+++ src/components/pages/homePage/CreativeEnvironment/useAnimate.js
@@ -8,94 +8,14 @@ export const scaleImgOnScroll = (elem) => {
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: elem,
-                start: '-25%',
-                end: '+=400',
-                scrub: 0.8,
+                start: '-20% top',
+                end: '20% top',
+                scrub: 1,
+                markers: true,
             },
         });
         tl.to(elem, {
-            clipPath: 'inset(0 0%)',
-            ease: 'ease-in-out',
-        })
-            .to(elem, {
-                clipPath: 'inset(0 .5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 1%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 1.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 2%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 2.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 3%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 3.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 4%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 4.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 5.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 6%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 6.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 7%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 7.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 8%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 8.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 9%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 9.5%)',
-                ease: 'ease',
-            })
-            .to(elem, {
-                clipPath: 'inset(0 10%)',
-                ease: 'ease',
-            });
+            clipPath: 'inset(0 10%)',
+        });
     }
 };
